964690293c495890264cf0a2be10e8d8a220b2cd
phob0z <leonel.alfonso@gmail.com>
2022-11-18 00:16:40 -0500

    ENV on Logout

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -27,7 +27,7 @@ const Menu = () => {
     try {
       await axios.post(
         // "http://127.0.0.1:8000/api/logout",
-        `${process.env.REACT_APP_BACK_URL}/logout`,
+        `${process.env.REACT_APP_BACK_URL}/logout/`,
         {},
         { headers: { accept: "application/json", authorization: token } }
       );
