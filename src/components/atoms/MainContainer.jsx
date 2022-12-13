5851a53922b5ffba233390118a322fe28fe646cf
phob0z <leonel.alfonso@gmail.com>
2022-12-13 12:49:37 -0500

    Fix del MainContainer/Cards

--- src/components/atoms/MainContainer.jsx
+++ src/components/atoms/MainContainer.jsx
@@ -4,17 +4,26 @@ import classes from "./MainContainer.module.css";
 
 function MainContainer(props) {
   return (
-    <div className={classes.container}>
-      <div className={classes.containerOptions} options={props.options}/>
-      <Card label="Nombre"/>
-      <Card label="Apellido"/>
-      <Card label="ASDASDASD"/>
-      <Card label="QWEWQeqwe"/>
-      <Card label="NASD"/>
-      <Card label="ZXCZXCZXC"/>
-      <Card label="ASDASD"/>
-      <Card label="AZXZXCC"/>
-      <Card label="ZXCASD"/>
+    <div className={classes.mainContainer}>
+      <div className={classes.subContainer}>
+        <div className={classes.containerOptions} options={props.options} />
+        <Card label="Nombre" />
+        <Card label="Apellido" />
+        <Card label="ASDASDASD" />
+        <Card label="QWEWQeqwe" />
+        <Card label="NASD" />
+        <Card label="ZXCZXCZXC" />
+        <Card label="ASDASD" />
+        <Card label="AZXZXCC" />
+        <Card label="ZXCASD" />
+        <Card label="ASDASD" />
+        <Card label="AZXZXCC" />
+        <Card label="ZXCASD" />
+        <Card label="ASDASD" />
+        <Card label="AZXZXCC" />
+        <Card label="ZXCASD" />
+        <Card label="ASDASD" />
+      </div>
     </div>
   );
 }
